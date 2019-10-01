7430c7464e9d2b78b483e69c43ae3b471053ac22
kayerosacia <chayegt@gmail.com>
2019-10-01 09:09:00 +0800

    Changed Variable in database

--- database/migrations/2019_06_28_040311_create_guests_table.php
+++ database/migrations/2019_06_28_040311_create_guests_table.php
@@ -1,44 +1,41 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateGuestsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('guests', function (Blueprint $table) {
-            $table->increments('id');
-            $table->double('student_number', 11, 0);
-            $table->string('last_name');
-            $table->string('first_name');
-            $table->string('middle_initial')    
-            ->nullable();
-            $table->enum('college', [
-                'law', 'dent', 'cas', 'cba',
-                'eng', 'ccss', 'educ', 'cfad',
-            ]);
-            $table->string('course');
-            $table->integer('year_level');
-            $table->double('contact_number', 11, 0)
-            ->nullable();
-            $table->timestamps();
-        });
-    }
+class CreateGuestsTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('guests', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('student_number', 11, 0);
+			$table->string('last_name');
+			$table->string('first_name');
+			$table->string('middle_initial')
+				->nullable();
+			$table->enum('college', [
+				'law', 'dent', 'cas', 'cba',
+				'eng', 'ccss', 'educ', 'cfad',
+			]);
+			$table->string('course');
+			$table->string('year_level');
+			$table->string('contact_number', 11, 0)
+				->nullable();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('guests_tables');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('guests_tables');
+	}
 }
